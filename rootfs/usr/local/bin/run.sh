934847d1987d259a2f2699cf8357f049a884f8bb
Rathorian <nataniel.perissier@gmail.com>
2019-04-29 21:20:00 +0200

    Small improvements

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -3,10 +3,10 @@
 sed -i -e "s|<UPLOAD_MAX_SIZE>|$UPLOAD_MAX_SIZE|g" /etc/nginx/nginx.conf /etc/php7/php-fpm.conf \
        -e "s|<MEMORY_LIMIT>|$MEMORY_LIMIT|g" /etc/php7/php-fpm.conf \
        -e "s|<CRON_PERIOD>|$CRON_PERIOD|g" /etc/s6.d/cron/run \
-       -e "s|<TIMEZONE>|$TIMEZONE|g" /etc/php7/php-fpm.conf
+       -e "s|<TZ>|$TZ|g" /etc/php7/php-fpm.conf
 
-ln -snf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-echo $TIMEZONE > /etc/timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
 
 if [ ! -d /etc/php7/session ]; then
   mkdir /etc/php7/session;
